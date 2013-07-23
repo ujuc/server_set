49d4ac57cafe5d9c504e0662534a53ecfd1eefb9
Gang Sung Jin <ujuc@ujuc.kr>
2013-07-24 01:52:18 +0900

    add README.md server_set.sh

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ubuntu Server Setting
+
+* zsh, vimrc setting
+
+
+# Resource
+
+* [oh-my-zsh](1)
+* [zsh-syntax-highlighing](2)
+* [amix/vimrc](3)
+* [vim-markdown](4)
+
+(1):https://github.com/robbyrussell/oh-my-zsh
+(2):https://github.com/zsh-users/zsh-syntax-highlighting
+(3):https://github.com/amix/vimrc
+(4):https://github.com/tpope/vim-markdown
---
+++ server_set.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#############################
+## zsh, vimrc setting      ##
+## By. ujuc (ujuc@ujuc.kr) ##
+#############################
+
+function printMessage() {
+    echo -e "\e[1;37m# $1\033[0m"
+}
+
+function install_git {
+    echo ""
+    printMessage "install git"
+    sudo apt-get install git-core -y
+}
+
+function install_zsh {
+    echo ""
+    printMessage "install zsh"
+    sudo apt-get install zsh -y
+}
+
+function setting_zsh {
+    echo ""
+    printMessage "setting_zsh"
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    mkdir ~/.oh-my-zsh/custom/plugins
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git  ~/.oh-my-zsh/custom/plugins
+    cp ~/.zshrc ~/.zshrc.orig
+    cat > ~/.zshrc
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="cloud"
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how many often would you like to wait before auto-        updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting   for completion
+# COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/  plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git python tmux screen pip zsh-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
+
+# Color listing
+#eval $(dircolors ~/.dir_colors)
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+    chsh -s /bin/zsh
+    source ~/.zshrc
+}
+
+function install_vim {
+    echo ""
+    pirntMessage "install vim"
+    sudo apt-get install vim
+}
+
+function setting_vim {
+    echo ""
+    git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+}
+
+install_git
+install_zsh
+setting_zsh
+install_vim
+setting_vim
+
+echo ""
+echo "Discounneted ssh or logout"
+
